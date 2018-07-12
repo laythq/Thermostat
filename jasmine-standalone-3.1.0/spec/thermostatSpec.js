f549c60ae3592b607f9d77b7d35f578931f87a64
Layth <laythq@gmail.com>
2018-07-12 11:25:47 +0100

    Add Power Saver function

--- jasmine-standalone-3.1.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/thermostatSpec.js
@@ -16,6 +16,28 @@ describe('up', function() {
     thermostat.up(1)
     expect(thermostat.returnTemperature()).toEqual(21)
   })
+
+  it('cannot go above maximum temperature', function() {
+    thermostat.powerSaver("on")
+    expect( function() { thermostat.up(6) }).toThrow('Maximum temperature breached')
+  })
+})
+
+describe('power saving mode: on', function() {
+  it('sets maximum temperature to 25 degrees', function() {
+    thermostat.powerSaver("on")
+    expect(thermostat.maximumTemperature).toEqual(25)
+  })
+  it('is on by default', function() {
+    expect(thermostat.maximumTemperature).toEqual(25)
+  })
+})
+
+describe('power saving mode: off', function() {
+  it('sets maximum temperature to 32 degrees', function() {
+    thermostat.powerSaver("off")
+    expect(thermostat.maximumTemperature).toEqual(32)
+  })
 })
 
 describe('down', function() {
--- jasmine-standalone-3.1.0/src/thermostat.js
+++ jasmine-standalone-3.1.0/src/thermostat.js
@@ -1,6 +1,7 @@
 var Thermostat = function() {
   this.temperature = 20
   this.minimumTemperature = 10
+  this.maximumTemperature = 25
 }
 
 Thermostat.prototype.returnTemperature = function() {
@@ -8,6 +9,9 @@ Thermostat.prototype.returnTemperature = function() {
 }
 
 Thermostat.prototype.up = function(value) {
+  if ((this.temperature + value) > this.maximumTemperature) {
+    throw "Maximum temperature breached"
+  }
   this.temperature = this.temperature + value
 }
 
@@ -16,4 +20,15 @@ Thermostat.prototype.down = function(value) {
     throw "Minimum temperature is 10 degrees"
   }
   this.temperature = this.temperature - value
+}
+
+Thermostat.prototype.powerSaver = function(command) {
+  if (command === "on") {
+    this.maximumTemperature = 25
+  }
+
+  else if (command === "off") {
+    this.maximumTemperature = 32
+  }
+  else throw "Invalid command, choose on or off"
 }
